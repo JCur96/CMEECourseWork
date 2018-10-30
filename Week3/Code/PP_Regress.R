7a8a2f7249732f4307033a8475e56ac61ce4760f
Jake <jake.curry96@gmail.com>
2018-10-30 15:26:04 +0000

    Finished off PP_Regrees (I think!)

--- Week3/Code/PP_Regress.R
+++ Week3/Code/PP_Regress.R
@@ -13,112 +13,70 @@ require(dplyr)
 require(tidyr)
 
 #### Reading in the data
-ppd <- read.csv("../Data/EcolArchives-E089-51-D1.csv") 
-head(ppd)
+raw <- read.csv("../Data/EcolArchives-E089-51-D1.csv") 
+head(raw)
+
+##### Standardising the data - all prey mass in grams
+ppd <- raw %>% rowwise() %>% mutate(Prey.mass = ifelse(Prey.mass.unit == "mg", Prey.mass/1000, Prey.mass)) # if prey.mass.unit =="mg" 
+# then replace prey.mass with prey.mass/1000 
+
+ppd[,"Prey.mass.unit"] = "g" # replaces all the mass units with g as all are now grams
 
 #### plotting 
-
 p <- qplot(data = ppd, x = Prey.mass,
            y = Predator.mass,
            log = "xy",
-           main = "Relation between predator and prey mass",
            xlab = "Prey mass (g)", 
            ylab = "Predator mass (g)",
            shape = I(3),
            facets = Type.of.feeding.interaction ~ .,
            colour = Predator.lifestage) + 
-           geom_point(shape = I(3))
-p + geom_smooth(method =lm, fullrange=T) + theme_bw()
+  geom_point(shape = I(3))
+p + geom_smooth(method =lm, fullrange=T) + theme_bw() + 
+  theme(legend.title = element_text(face = "bold"), legend.position = "bottom", plot.margin = unit(c(1,4,1,4), "cm")) + 
+  guides(colour = guide_legend(nrow = 1))
+
+
+##### Saving the graph to a pdf cause why not 
+pdf("../Results/PP_Regress_Results.pdf", # Open blank pdf page using a relative path
+    11.7, 8.3) # These numbers are page dimensions in inches
+p <- qplot(data = ppd, x = Prey.mass,
+           y = Predator.mass,
+           log = "xy",
+           xlab = "Prey mass (g)", 
+           ylab = "Predator mass (g)",
+           shape = I(3),
+           facets = Type.of.feeding.interaction ~ .,
+           colour = Predator.lifestage) + 
+  geom_point(shape = I(3))
+p + geom_smooth(method =lm, fullrange=T) + theme_bw() + 
+  theme(legend.title = element_text(face = "bold"), legend.position = "bottom", plot.margin = unit(c(1,4,1,4), "cm")) + 
+  guides(colour = guide_legend(nrow = 1))
+graphics.off(); #you can also use dev.off()
 
 
 ##### getting results of lm 
 # run a linear model of log prey mass ~ log predator mass 
 # but by subset of feeding type and life stage (i.e. juv predatios, adult predatious etc.)
 
-combinations = expand.grid(unique(ppd$Type.of.feeding.interaction), unique(ppd$Predator.lifestage)) # make a df of all possible combinatation
-for (i in 1:nrow(combinations)){
-  tfi = combinations[[i, "Var1"]]
-  pls = combinations[[i, "Var2"]]
-  subset_model = subset(ppd, Type.of.feeding.interaction == tfi & Predator.lifestage == pls)
-  print(head(subset_model))
-}
-
-
-
-# this works I think 
 list_reg <- dlply(ppd, .(Type.of.feeding.interaction, Predator.lifestage), function(ppd) 
 {lm(log(Predator.mass)~log(Prey.mass),data=ppd)})
 
-c <- c("Type.of.feeding.interaction", "Predator.lifestage")
 
-list_reg <- dlply(ppd, c, function(df) 
-{lm(log(Predator.mass)~log(Prey.mass),data=df)
+outStat <- ldply(list_reg, function(df){ # Makes new df 
+  intercept = summary(df)$coefficients[1] # pulls out the stats from the summary matrix 
+  slope = summary(df)$coefficients[2] # each [] intger represents a differnt column in the stats matrix
+  r2 = summary(df)$r.squared # is a pain so has be be pulled out slightly differently
+  pvalue = summary(df)$coefficients[8]
+  data.frame(intercept, slope, r2, pvalue)
   })
 
-length(list_reg)
-#That's how you check out one particular regression, in this case the first
-summary(list_reg[[1]])
 
-list_reg
+fstat <- ldply(list_reg, function(df){fStat = summary(df)$fstatistic[1] # is a right pain as it breaks the code if in the outStat bit
+         data.frame(fStat)}) # so is kept out here 
 
-coefs = ldply(list_reg, coef) # pull out parts of the reg_list results
-
-R2 = ldply(list_reg, r.squared)
-
-coefs
-
-list_r2 <- dlply(ppd, function(df){summary(lm(log(Predator.mass)~log(Prey.mass),data=ppd))$r.squared})
-  
-
-# model1<- lm(log(ppd$Predator.mass) ~ log(ppd$Prey.mass), data = subset(ppd, Type.of.feeding.interaction, Predator.lifestage))
-# 
-# lm(y ~ x + z, data=subset(myData, sex=="female"))
-# 
-# summary(model1)
-
+finaldf <- merge(outStat, fstat) # merging the two results frames
 
 #### Making a results csv file
-write.csv(MyDF, "../Results/PP_Regress_Results.csv", row.names = F) # writing out the final df
+write.csv(finaldf, "../Results/PP_Regress_Results.csv", row.names = F) # writing out the final df
 # and removing the indexing column
-
-
-
-
-
-
-
-
-# p <- ggplot(ppd, aes(x = Prey.mass,
-#                      y = Predator.mass,
-#                      log = "xy",
-#                      main = "Relation between predator and prey mass",
-#                      xlab = "Prey mass (g)", 
-#                      ylab = "Predator mass (g)",
-#                      shape = I(3)) + facet_grid(Type.of.feeding.interaction ~ .) +
-#                      aes(colour = Predator.lifestage) + geom_point())
-# 
-# pf <- p + facet_grid(Type.of.feeding.interaction ~ .) # put these inside the main plot 
-# pf + aes(colour = Predator.lifestage) + geom_point() # it might sort the labelling issue
-
-# scale_y_continuous() # this might be how we label the axis ticks correclty (remember it is the log so the weight in grams will be x+-*10^)
-
-# pfp <- pf + coord_cartesian(xlim = c(1e-07, 1e+01), ylim = c(1e-06, 1e+06), expand = TRUE) + geom_point() # this bit isnt helping
-# just need to do the tick marks and scale 
-
-# ggplot(subset(dat,ID %in% c("P1" , "P3"))) + # can use subset in ggplot, this might be how we do this individually
-  # facets =  .~ Type.of.feeding.interaction, # Or facets might be the way to go - this one seems better to me as less writing
-
-# p1 <- ggplot(Prey.mass, Predator.mass, data = ppd, geom = c("point", "smooth"), # need to make it shape= I(3)) I think to get the crosses
-       # colour = Predator.lifestage + geom_smooth(method = "lm")) # Type.of.feeding.interaction) 
-
-
-# t1 <- qplot(Prey.mass, Predator.mass, data = ppd, facets =  .~ Type.of.feeding.interaction, facet_grid(method~., scale='free', space='free')), colour = Predator.lifestage)
-# t2 <- t1 + coord_fixed()
-# t2
-
-
-
-# coord_cartesian(ylim = xlim))) # for fixing scale, but doesnt seem to do anything, might need 
-# scale_y_continuous() # beforehand to make it do its thing
-
-# multiplot(p1, p2, p3, p4, p5, cols=1) # makes a single page, one plot on top of the other #facets should do this also
